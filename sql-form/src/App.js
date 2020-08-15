3c1855d910d0c6891ec88800236dd974c0f5b276
Yaroslav <pijuwei2@gmail.com>
2020-08-15 14:59:10 +0300

    remove btn, add fetch

--- sql-form/src/App.js
+++ sql-form/src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import './App.css';
+import Checkbox from './Checkbox'
+import Amaunt from './Amount';
 
 class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      products: []
+      products: [],
+      checkboxes: []
     }
+    this.changeState = this.changeState.bind(this)
+  }
+  
+  changeState (event) {
+    const {checkboxes} = this.state
+    const arr = checkboxes.filter(({id}) => id === event.target.id)
+    if (arr.length) {
+      this.setState({checkboxes:[...checkboxes.filter(({id}) => id !== event.target.id)]})
+      return
+    }
+
+    this.setState({checkboxes: [...checkboxes, {id: event.target.id, price: event.target.attributes.price.value }]})
+    console.log(this.state.checkboxes)
   }
 
-  componentDidMount () {
+  componentDidMount = _ => {
     this.getProducts()
   }
 
@@ -20,13 +36,34 @@ class App extends React.Component {
     .catch(err => console.error(err))
   }
 
-renderProduct = ({name, price}) => <div key={Math.round(Math.random() * name.length * price)}>{name + " " + price}</div> 
+  addProduct = _ => {
+    (this.state.checkboxes.length > 0) ? 
+    this.state.checkboxes.map(({id}) => 
+      fetch(`http://localhost:4000/products/add?id=${id}`)
+      .then(() => console.log ( '%c%s', 'color: green;', 'product added' ))
+      .catch(err => console.error(err))  
+    ): 
+    console.error('err')
+  }
+
+  onChecked = event => {
+    this.setState({rememberMe: event.target.checked})
+    console.log(event.target.checked)
+  }
 
   render () {
-    const {products} = this.state
+    const {products, checkboxes} = this.state
+    const {changeState} = this
     return (
       <div className="App">
-        {products.map(this.renderProduct)}
+        <div className='Checkboxes'>
+        {products.map((obj) => {
+         return (
+           <Checkbox  key={obj.id} {...obj} OnChengeCheck={changeState}></Checkbox>
+         )
+        })}
+        </div> 
+        <Amaunt totalPrice={checkboxes.reduce((state, curr) => state += parseInt(curr.price), 0)} addProduct={this.addProduct}></Amaunt>
       </div>
     )
   }
